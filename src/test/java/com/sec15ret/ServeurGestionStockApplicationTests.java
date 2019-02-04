eefa1117f11b6ebde730899f2b0903f7302b655c
Goulzaraly <un.sec15ret@gmail.com>
2019-02-04 11:53:59 +0300

    first commit

---
+++ src/test/java/com/sec15ret/ServeurGestionStockApplicationTests.java
@@ -0,0 +1,17 @@
+package com.sec15ret;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServeurGestionStockApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
